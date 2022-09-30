b878242ef8ed14633e64c91e3cdf21c0cd825231
saeed shargi ghazani <saeed.shargi.gh@outlook.com>
2022-09-30 20:33:38 +0330

    add Error value object and add Result class to use for domain exception

---
+++ src/Benim/Domain/Common/Result.cs
@@ -0,0 +1,33 @@
+﻿using Benim.Domain.ValueObjects;
+
+namespace Benim.Domain.Common;
+
+public class Result<TValue> where TValue : class
+{
+    private readonly TValue? _value;
+
+    private Result(TValue value)
+    {
+        IsSuccess = true;
+        Error = Error.None;
+        _value = value;
+    }
+
+    private Result(Error error)
+    {
+        if (error == Error.None)
+        {
+            throw new InvalidOperationException("The error cant not be empty.");
+        }
+        IsSuccess = false;
+        Error = error;
+        _value = default(TValue);
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    public TValue? Value => IsSuccess ? _value : null;
+    public static Result<TValue> Success(TValue value) => new(value);
+    public static Result<TValue> Failure(Error error) => new(error);
+}
---
+++ src/Benim/Domain/ValueObjects/Error.cs
@@ -0,0 +1,26 @@
+﻿using Benim.Domain.Common;
+
+namespace Benim.Domain.ValueObjects;
+
+public class Error : ValueObject
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+    public string Message { get; }
+
+    public static implicit operator string(Error error) => error.Code;
+
+    protected override IEnumerable<object> GetValues()
+    {
+        yield return Code;
+        yield return Message;
+    }
+}
